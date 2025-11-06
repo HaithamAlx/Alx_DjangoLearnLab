2bb20855bb8eee3d02b5d800b4ae19cd15aed19e
Haitham <hmohamed.r1123@gmail.com>
2025-11-06 10:54:31 -0800

    task 2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,8 +9,48 @@ from django.contrib.auth import login
 from django.contrib.auth import logout
 
 
+# def list_books(request):
+#     books = Book.objects.all()  
+#     return render(request, 'relationship_app/list_books.html', {'books': books})
+
+
+# class LibraryDetailView(DetailView):
+#     model = Library
+#     template_name = 'relationship_app/library_detail.html'
+#     context_object_name = 'library'
+
+
+# def register_view(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)  
+#             return redirect('list_books')  
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'relationship_app/register.html', {'form': form})
+
+
+# def login_view(request):
+#     if request.method == 'POST':
+#         form = AuthenticationForm(data=request.POST)
+#         if form.is_valid():
+#             user = form.get_user()
+#             login(request, user)
+#             return redirect('list_books')
+#     else:
+#         form = AuthenticationForm()
+#     return render(request, 'relationship_app/login.html', {'form': form})
+
+
+# def logout_view(request):
+#     logout(request)
+#     return render(request, 'relationship_app/logout.html')
+
+
 def list_books(request):
-    books = Book.objects.all()  
+    books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
@@ -25,8 +65,8 @@ def register_view(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  
-            return redirect('list_books')  
+            login(request, user)
+            return redirect('list_books')
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
