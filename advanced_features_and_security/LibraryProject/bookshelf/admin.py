af686519a56fa0419825fc019f6cabdc8570fc35
Haitham <hmohamed.r1123@gmail.com>
2025-11-13 13:19:24 -0800

    2nd upload

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 from .models import Book
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from bookshelf.models import Book
+
+# Define groups
+groups = {
+    "Admins": ["can_view", "can_create", "can_edit", "can_delete"],
+    "Editors": ["can_create", "can_edit"],
+    "Viewers": ["can_view"]
+}
+
+book_content_type = ContentType.objects.get_for_model(Book)
+
+for group_name, perms in groups.items():
+    group, created = Group.objects.get_or_create(name=group_name)
+    for perm_codename in perms:
+        perm = Permission.objects.get(codename=perm_codename, content_type=book_content_type)
+        group.permissions.add(perm)
+
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Book
+from .forms import BookForm
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'bookshelf/edit_book.html', {'form': form})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/create_book.html', {'form': form})
