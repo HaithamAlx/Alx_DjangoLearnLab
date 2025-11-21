09b21438a04cccda168b6be7e153a3184165a01c
Haitham <hmohamed.r1123@gmail.com>
2025-11-21 08:11:17 -0800

    API

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from rest_framework import serializers
+from .models import Book
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
@@ -7,3 +9,11 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.author}"
+
+
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
