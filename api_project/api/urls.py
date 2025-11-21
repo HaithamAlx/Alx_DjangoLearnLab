403ad82f9db448a38d4810fe951d9492f3d21fdd
Haitham <hmohamed.r1123@gmail.com>
2025-11-21 08:13:19 -0800

    API

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import BookListAPIView
+from .views import BookList
 
 urlpatterns = [
-    path('books/', BookListAPIView.as_view(), name='book_list'),
+    path('books/', BookList.as_view(), name='book_list'),
 ]
