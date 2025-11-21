25ba2432d04d0468466819b0aaf8e77d409fb177
Haitham <hmohamed.r1123@gmail.com>
2025-11-21 08:15:09 -0800

    API

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,23 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
-class BookListAPIView(generics.ListAPIView):
+
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for managing CRUD operations on the Book model.
+    Provides:
+    - list: GET /books_all/
+    - retrieve: GET /books_all/<id>/
+    - create: POST /books_all/
+    - update: PUT /books_all/<id>/
+    - partial_update: PATCH /books_all/<id>/
+    - destroy: DELETE /books_all/<id>/
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
